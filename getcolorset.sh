c92a58917668fb17bf9509cd3a09c0847b2d046b
root <root@pi-star>
2024-12-13 15:59:40 -0500

    update

--- getcolorset.sh
+++ getcolorset.sh
@@ -9,50 +9,50 @@
 set -o errexit
 set -o pipefail
 
-#if [ -z "$2" ]; then
-#	SetNum="$1"
-#else
-#   SetNum=$(sed -n '/^[ \t]*\['"$2"'\]/,/\[/s/^[ \t]*SetNum[ \t]*=[ \t]*//p' /etc/Colors.ini)
-#fi;
+if [ "$2" ]; then
+	if [ "$2" = "MMDVM" ];  then
+		m1=$(sed -n '/^[ \t]*\[MMDVM\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
 
-if [ "$2" = "MMDVM" ];  then
-	m1=$(sed -n '/^[ \t]*\[MMDVM\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
+	if [ "$2" == "DMR" ]; then
+		m1=$(sed -n '/^[ \t]*\[DMR\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
 
-if [ "$2" == "DMR" ]; then
+	if [ "$2" == "YSF" ]; then
+		m1=$(sed -n '/^[ \t]*\[YSF\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
 
-	m1=$(sed -n '/^[ \t]*\[DMR\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
+	if [ "$2" == "P25" ]; then
+		m1=$(sed -n '/^[ \t]*\[P25\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
 
-if [ "$2" == "YSF" ]; then
-	m1=$(sed -n '/^[ \t]*\[YSF\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
+	if [ "$2" == "NXDN" ]; then
+		m1=$(sed -n '/^[ \t]*\[NXDN\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
 
-if [ "$2" == "P25" ]; then
-	m1=$(sed -n '/^[ \t]*\[P25\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
+	if [ "$2" == "D-Star" ]; then
+		m1=$(sed -n '/^[ \t]*\[D-Star\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
 
-if [ "$2" == "NXDN" ]; then
-	m1=$(sed -n '/^[ \t]*\[NXDN\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
+	if [ "$2" == "M17" ]; then
+		m1=$(sed -n '/^[ \t]*\[M17\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
+	fi
+	SetNum="$m1"
+	
+    else
+	SetNum="$1"
 
-if [ "$2" == "D-Star" ]; then
-	m1=$(sed -n '/^[ \t]*\[D-Star\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
-
-if [ "$2" == "M17" ]; then
-	m1=$(sed -n '/^[ \t]*\[M17\]/,/\[/s/^[ \Set[ \t]*=[ \t]*//p' /etc/Colors.ini)
-fi
-
-
-SetNum="$m1"
+    fi
 
 m1=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*UpperBack[ \t]*=[ \t]*//p' /etc/Colors.ini)
 m2=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*UpperText[ \t]*=[ \t]*//p' /etc/Colors.ini)
 m3=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*LowerBack[ \t]*=[ \t]*//p' /etc/Colors.ini)
 m4=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*LowerText[ \t]*=[ \t]*//p' /etc/Colors.ini)
+
+#Split
 m5=$(sed -n '/^[ \t]*\[ColorSet'"$SetNum"'\]/,/\[/s/^[ \t]*Split[ \t]*=[ \t]*//p' /etc/Colors.ini)
+
 #m5=$(sed -n '/^[ \t]*\[General\]/,/\[/s/^[ \t]*Split[ \t]*=[ \t]*//p' /etc/Colors.ini)
 mt="$SetNum|$m5|$m1|$m2|$m3|$m4"
 echo "$mt"
-echo "$m1"
+#echo "$m1"
